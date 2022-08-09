b90fc1d5d645fc063b56d63960a6786883edf96f
Wayne Tsai <wtsai45@gmail.com>
2022-08-08 17:10:51 -0700

    added search bar for homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-
+import SearchBar from '../../components/SearchBar/SearchBar'
+// import Navbar from '../../components/Navbar/Navbar.jsx'
 import './Homepage.css'
 
 export default function Homepage () {
 
-  const applications = useSelector((state) => {
-    return state.app.applications
-  })
-  console.log(applications)
+  // const applications = useSelector((state) => {
+  //   return state.app.applications
+  // })
+  // console.log(applications)
   return (
     <div id='home'>
       <div>
+        <SearchBar/>
       </div>
       <div id='content'>
       </div>
